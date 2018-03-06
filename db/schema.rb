d9b66517831a2653db45ffb0670232e3c86aeb15
Elijah Omolo <eomolo11@gmail.com>
2018-03-06 08:57:46 -0600

    add associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305162643) do
+ActiveRecord::Schema.define(version: 20180306143446) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "desription"
     t.datetime "create_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

5a0546bd1a62d5044fd821f3c8ba4ac7354ee34f
Elijah Omolo <eomolo11@gmail.com>
2018-03-14 09:28:31 -0500

    signup from UI

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :articles 
+  resources :articles
+
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 
 end
